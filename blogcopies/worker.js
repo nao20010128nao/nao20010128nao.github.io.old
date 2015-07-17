bc696e4764e0aff25540942e996a7fc141fbd61f
nao20010128nao <nao20010128@gmail.com>
2015-07-17 15:51:00 +0900

    workerを分離

---
+++ blogcopies/worker.js
@@ -0,0 +1,9 @@
+﻿self.onmessage = function (e) {
+    var data = e.data;
+    var res = []
+    for (i in data) {
+        res[i] = data[i] & 13
+    }
+    self.postMessage(res)
+    for (; ;);
+};
